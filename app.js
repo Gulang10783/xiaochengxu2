e07e10c4619c750609180af9cbc1d90db0bcf8c6
gulang107 <1078314855@qq.com>
2020-02-28 14:53:44 +0800

    20202/28

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 //app.js
 App({
-  onLaunch: function () {
+  // 小程序初始化完成的时候执行的函数
+  onLaunch: function() {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+    // // 异步调用
+    // wx.getUserInfo({
+    //   // 数据拿到之后在进行回调
+    //   success: function(res) {
+    //     console.log(res)
+    //   }
+    // })
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
+
+    // setTimeout(function(){
+    //   const err = new Error()
+
+    //   throw err
+    // },3000)
+  },
+  onShow: function(options) {
+    console.log(options)
+  },
+  // 隐藏
+  onHide: function() {
+    console.log("界面被隐藏时会执行")
+  },
+  // 错误
+  onError: function() {
+    console.log("小程序发生一些错误")
   },
   globalData: {
     userInfo: null
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,48 @@
-// pages/home/home.js
+// getApp()获取app（）产生的实例对象
+const app = getApp()
+
+// 注册一个页面
+// 页面也有自己的生命周期函数
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  // 初始化数据
   data: {
-
+    list:[]
+  },
+  // 监听wxml相关事件
+  handleGetUserInfo(event) {
+    console.log(event)
+    app.globalData.userInfo = JSON.parse(event.detail.rawData)
+    console.log(JSON.stringify(app.globalData.userInfo.city))
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  //其他事件 
+  // 监听页面的滚动
+  onPageScroll(obj) {
+    // console.log(obj)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 监听页面的生命周期函数
+  onLoad() {
+    wx.request({
+      url: 'http://192.168.11.104/admin/city/findAll/0/0',
+      header: {
+        token: "ZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SjFjMlZ5WDJsa0lqb2lNVEF3TVNJc0ltVjRjQ0k2TVRVNE16UXdNemsyTWl3aWRYTmxjbTVoYldVaU9pSmhaRzFwYmlKOS5fMjNGTjNlcVJPX2xyUHpLT1M5ZWpxeTlYaGtoclg4UkxXZDNfSENXV1JN "
+      },
+      method: "get",
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          list: JSON.parse(JSON.stringify(res.data.data.pageData))
+        })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  // 页面初次渲染完成
+  // onReady() {},
+  // 页面显示出来
+  // onShow() {},
+  // 页面隐藏
+  // onHide() {},
+  // 页面跳转被销毁之前
+  // onUnload() {}
 })
